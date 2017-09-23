59e0fb45c03674556aff575e61031676bc641ccd
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-23 17:29:03 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  counter = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      then counter += 1
+end
+end
+return counter
+end
+
+def current_player(board)
+  
