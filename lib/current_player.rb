7b60bb6787fba8c8b7fca038ea0a3bcd3c35bd30
Daniel Rios <danielrafaelrios@gmail.com>
2017-09-23 17:57:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,14 +9,10 @@ return counter
 end
 
 def current_player(board)
-counter = 0
-board.each do |move|
-  counter = gets.chomp.to_i
-  counter += 1
-  if counter.even?
-    puts "O"
-  else counter.odd?
-    puts "X"
-  end
+if (turn_count(board) % 2) != 0
+  puts "O"
+else
+  puts "X"
 end
 end
+
